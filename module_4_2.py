8c1417bb443c906c294b5d8bbe45c8a322482fda
Vladickush <u.vn@mail.ru>
2024-08-18 20:54:57 +0300

    Add files via upload

---
+++ module_4_2.py
@@ -0,0 +1,10 @@
+# Домашнее задание по уроку "Пространство имен"
+def test_function():
+    def inner_function():
+        print('Я в области видимости функции test_function')
+
+    inner_function()
+
+
+test_function()
+# inner_function() #NameError: name 'inner_function' is not defined. Did you mean: 'test_function'?
